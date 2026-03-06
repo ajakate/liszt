e792e4ce55c6d1fe9f9badad7942b20f45257db5
Ajay Jakate <ajay.jakate@gmail.com>
2026-03-05 23:22:21 -0600

    dont open devtools automatically

--- electron/main.ts
+++ electron/main.ts
@@ -20,7 +20,6 @@ function createWindow() {
 
   if (process.env.NODE_ENV === 'development' || process.env.VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL || 'http://localhost:5173');
-    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
   }
